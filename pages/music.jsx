3d555234233d30d87aae768fcade9b46b1416311
faruk-ahmed10 <farukahmed31b32@gmail.com>
2022-02-26 08:46:58 +0600

    Updated

--- pages/music.jsx
+++ pages/music.jsx
@@ -2,22 +2,58 @@ import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import banner from '../assets/img/banner.png';
 import banner2 from '../assets/img/banner2.png';
+import musictag from '../assets/img/musictag.png';
+import musictag2 from '../assets/img/musictag2.png';
 import Link from 'next/link';
 
 import { BsShuffle, BsFillHeartFill, BsThreeDotsVertical } from 'react-icons/bs';
 import { ImLoop } from 'react-icons/im';
 import { IoPlaySkipForwardSharp, IoPlaySkipBackSharp, IoPlaySharp, IoPause } from 'react-icons/io5';
 import { RiPlayList2Fill } from 'react-icons/ri';
-import { MdOutlineDevicesOther } from 'react-icons/md';
+import { MdOutlineDevicesOther, MdDone } from 'react-icons/md';
 import { GiSpeaker } from 'react-icons/gi';
-import { AiOutlineExpandAlt } from 'react-icons/ai';
+import { AiOutlineExpandAlt, AiOutlinePlus } from 'react-icons/ai';
 
+
+const musicData = [
+    {
+        title: 'Colourblind',
+        imgUrl: musictag,
+        audioUrl: 'http://localhost:3000/bensound-dubstep.mp3',
+        views: 1812421
+    },
+    {
+        title: 'No Parallals',
+        imgUrl: musictag2,
+        audioUrl: 'http://localhost:3000/bensound-dubstep.mp3',
+        views: 1812421
+    },
+    {
+        title: 'Division Symbols',
+        imgUrl: musictag,
+        audioUrl: 'http://localhost:3000/bensound-dubstep.mp3',
+        views: 1812421
+    },
+    {
+        title: 'I am',
+        imgUrl: musictag,
+        audioUrl: 'http://localhost:3000/bensound-dubstep.mp3',
+        views: 1812421
+    },
+    {
+        title: 'Drift',
+        imgUrl: musictag,
+        audioUrl: 'http://localhost:3000/bensound-dubstep.mp3',
+        views: 1812421
+    },
+];
 
 const Music = () => {
 
     const [play, setPlay] = useState(false);
     const [volume, setVolume] = useState(50);
     const [currentTime, setCurrentTime] = useState(0);
+    const [selected, setSelected] = useState(null);
     const audio = useRef( typeof Audio !== "undefined" ? new Audio("http://localhost:3000/bensound-dubstep.mp3") : undefined );
     const [duration, setDuration] = useState();
     
@@ -50,19 +86,19 @@ const Music = () => {
                         <h1 className="text-2xl font-bold mb-3 text-dark-white">Popular</h1>
                         <ul className="flex flex-col gap-5">
                             {
-                                Array(5).fill('').map((v, idx) => (
+                                musicData.map((ms, idx) => (
                                     <li key={idx} className="w-full flex items-center text-sm">
                                         <div className="">
-                                            <Image src={banner} alt={'Music Tag'} width={50} height={50} />
+                                            <Image src={ms.imgUrl} alt={'Music Tag'} width={50} height={50} />
                                         </div>
                                         <div className="w-full flex">
-                                            <div className="">
-                                                <span className="px-2">1</span>
-                                                <span className="px-2">+</span>
+                                            <div className="flex items-center">
+                                                <span className="px-2">{idx}</span>
+                                                {selected ? <span className="px-2 text-sky-600 text-2xl"><MdDone/></span> : <span className="px-2 text-xl"><AiOutlinePlus/></span> }
                                             </div>
                                             <div className="flex flex-1">
-                                                <h3 className="flex-1 font-semibold text-dark-white">Colourblind</h3>
-                                                <h5 className="">1,812,421</h5>
+                                                <h3 className="flex-1 font-semibold text-dark-white">{ms.title}</h3>
+                                                <h5 className="">{ms.views}</h5>
                                             </div>
                                         </div>
                                     </li>
@@ -96,7 +132,7 @@ const Music = () => {
                     <h1 className="text-2xl font-bold text-dark-white pb-5">Albums</h1>
                     <div className="flex">
                         <div className="">
-                            <Image src={banner} alt={'Album Photo'} width={100} height={100} />
+                            <Image src={musictag} alt={'Album Photo'} width={100} height={100} />
                         </div>
                         <div className="pl-5 flex flex-col justify-between">
                             <h2 className="">2016</h2>
@@ -130,7 +166,7 @@ const Music = () => {
                                 <div className="w-full lg:w-36 h-1 bg-dark-pri/50 rounded-full overflow-hidden">
                                     <div className="w-3/5 h-full bg-dark-pri"></div>
                                 </div>
-                                <span className="pl-5 text-sm">{audio ? `${(duration / 60).toFixed(0)}:${((duration / 60) - (duration / 60).toFixed(0)).toFixed(2).replace(/^(0.)/,'')}` : 0}</span>
+                                <span className="pl-5 text-sm">{duration ? `${(duration / 60).toFixed(0)}:${((duration / 60) - (duration / 60).toFixed(0)).toFixed(2).replace(/^(0.)/,'')}` : 0}</span>
                             </div>
                         </div>
                         {/* Player Handler */}
